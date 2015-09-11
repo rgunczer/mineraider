92bc94d1f8f2e084425f6fbaf2b686a0fd3bfbc4
Robert Gunczer <rgunczer@gmail.com>
2015-09-11 12:38:11 +0000

    Overlay.java edited online with Bitbucket move touchDown vars to base class

--- app/src/main/java/com/almagems/mineraider/Overlay.java
+++ app/src/main/java/com/almagems/mineraider/Overlay.java
@@ -2,4 +2,7 @@ package com.almagems.mineraider;
 
 public abstract class Overlay {
     public static Graphics graphics;
+    
+    protected float touchDownX;
+    protected float touchDownY;    
 }

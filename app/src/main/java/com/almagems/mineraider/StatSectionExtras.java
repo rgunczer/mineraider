dc927122d78ca29e753bdfa59b75b73fcf97baf4
Robert Gunczer <rgunczer@gmail.com>
2015-09-15 13:25:17 +0000

    StatSectionExtras.java edited online with Bitbucket Replace getters with direct member variable access

--- app/src/main/java/com/almagems/mineraider/StatSectionExtras.java
+++ app/src/main/java/com/almagems/mineraider/StatSectionExtras.java
@@ -31,25 +31,25 @@ public final class StatSectionExtras extends StatSectionBase {
 
         y -= 0.275f;
 
-        texts[0].init("Shared Matches: " + scoreCounter.getSharedMatchCount(), textColor, textColor, fontScale);
+        texts[0].init("Shared Matches: " + scoreCounter.sharedMatchesCounter, textColor, textColor, fontScale);
         texts[0].pos.tx = x;
         texts[0].pos.ty = y;
         texts[0].posYorigin = texts[0].pos.ty;
 
         y -= 0.2f;
-        texts[1].init("Highest Combos: " + scoreCounter.getHighestComboCount(), textColor, textColor, fontScale);
+        texts[1].init("Highest Combos: " + scoreCounter.highestComboCounter, textColor, textColor, fontScale);
         texts[1].pos.tx = x;
         texts[1].pos.ty = y;
         texts[1].posYorigin = texts[1].pos.ty;
 
         y -= 0.2f;
-        texts[2].init("Perfect Swaps: " + scoreCounter.getPerfectSwapCount(), textColor, textColor, fontScale);
+        texts[2].init("Perfect Swaps: " + scoreCounter.perfectSwapCounter, textColor, textColor, fontScale);
         texts[2].pos.tx = x;
         texts[2].pos.ty = y;
         texts[2].posYorigin = texts[2].pos.ty;
 
         y -= 0.2f;
-        texts[3].init("Hints: " + scoreCounter.getHintCount(), textColor, textColor, fontScale);
+        texts[3].init("Hints: " + scoreCounter.hintCounter, textColor, textColor, fontScale);
         texts[3].pos.tx = x;
         texts[3].pos.ty = y;
         texts[3].posYorigin = texts[3].pos.ty;
